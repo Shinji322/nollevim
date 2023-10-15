e5bd6675b0ec42167874d27755d8eac6062a4486
Ronan Maharaj <81526911+Shinji322@users.noreply.github.com>
2023-10-14 23:14:42 -0400

    fix obsolete icons

--- config/completion.nix
+++ config/completion.nix
@@ -3,7 +3,7 @@
     enable = true;
     # preselect = "None";
     snippet.expand = "luasnip";
-    mappingPresets = ["insert" "cmdline"];
+    mappingPresets = [ "insert" "cmdline" ];
     mapping = {
       "<C-b>" = ''cmp.mapping.scroll_docs(-4)'';
       "<C-f>" = ''cmp.mapping.scroll_docs(4)'';
@@ -22,7 +22,7 @@
       "<C-e>" = "cmp.mapping.abort()";
       "<CR>" = "cmp.mapping.confirm({ select = true })";
       "<Tab>" = {
-        modes = ["i" "s"];
+        modes = [ "i" "s" ];
         action = ''
           function(fallback)
             if luasnip.jumpable(1) then
@@ -42,7 +42,7 @@
         '';
       };
       "<S-Tab>" = {
-        modes = ["i" "s"];
+        modes = [ "i" "s" ];
         action = ''
           function(fallback)
             if cmp.visible() then
@@ -86,11 +86,10 @@
     # };
 
     sources = [
-      # { name = "copilot"; }
-      {name = "luasnip";} #-- For luasnip users.
-      {name = "nvim_lsp";}
-      {name = "path";}
-      {name = "buffer";}
+      { name = "luasnip"; } #-- For luasnip users.
+      { name = "nvim_lsp"; }
+      { name = "path"; }
+      { name = "buffer"; }
     ];
 
     # confirm_opts = {
@@ -108,33 +107,33 @@
         return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
     end
 
-    --   פּ ﯟ   some other good icons
+    -- 󰃐 󰆩 󰙅 󰛡  󰅲 some other good icons
     kind_icons = {
-      Text = "",
+      Text = "󰉿",
       Method = "m",
-      Function = "",
+      Function = "󰊕",
       Constructor = "",
       Field = "",
-      Variable = "",
-      Class = "",
+      Variable = "󰆧",
+      Class = "󰌗",
       Interface = "",
       Module = "",
       Property = "",
       Unit = "",
-      Value = "",
+      Value = "󰎠",
       Enum = "",
-      Keyword = "",
+      Keyword = "󰌋",
       Snippet = "",
-      Color = "",
-      File = "",
+      Color = "󰏘",
+      File = "󰈙",
       Reference = "",
-      Folder = "",
+      Folder = "󰉋",
       EnumMember = "",
-      Constant = "",
+      Constant = "󰇽",
       Struct = "",
       Event = "",
-      Operator = "",
-      TypeParameter = "",
+      Operator = "󰆕",
+      TypeParameter = "󰊄",
     }
   '';
 
