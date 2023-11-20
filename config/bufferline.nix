4c116f17cf3792107baf6a857712254084a1cb36
Ronan Maharaj <81526911+Shinji322@users.noreply.github.com>
2023-11-20 17:14:30 -0500

    minimize bufferline

--- config/bufferline.nix
+++ config/bufferline.nix
@@ -1,13 +1,5 @@
 {
-  plugins.bufferline =
-    let
-      closeCommand = "Bdelete! %d";
-    in
-    {
-      inherit closeCommand;
-      enable = true;
-      rightMouseCommand = null;
-      middleMouseCommand = closeCommand;
-      indicator.icon = "▎";
-    };
+  plugins.bufferline = {
+    enable = true;
+  };
 }
